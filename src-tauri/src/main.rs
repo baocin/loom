46009efe2c511f837fa234fddfd73b3a781f7f51
Michael Pedersen <michaelp@discoverdst.com>
2025-02-02 15:28:32 -0600

    create loom.db on start

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    use std::path::Path;
+    let db_path = Path::new("loom.db");
+    if !db_path.exists() {
+        loom_app_lib::db::Database::new(db_path).expect("Failed to create database");
+    }
+    
     loom_app_lib::run()
 }
